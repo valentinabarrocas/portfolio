6be51c87b8294d2191bb01da719bb859c2f6f1c1
valentinabarrocas <valentinabarrocas@hotmail.com>
2026-02-04 15:31:13 -0300

    html, css and javascript finalized

--- script.js
+++ script.js
@@ -1,16 +1,29 @@
-// 1 -  pega todos os links dentro da lista
-const links = document.querySelectorAll('.list a');
+// seleciona os links
+const links = document.querySelectorAll('.project-link');
 
-// 2 - "para cada" link que você encontrou...
-links.forEach((cadaLink) => {
+// cria o loop
+links.forEach(link => {
+    link.addEventListener('click', (e) => {
+        e.preventDefault(); // Impede a página de pular pro topo
 
-    // 3 -  adicione o ouvinte de clique em cada um deles individualmente
-    cadaLink.addEventListener('click', (evento) => {
-        evento.preventDefault();
-        
-        // pega o texto do link que foi clicado para testar
-        const nomeDoProjeto = cadaLink.textContent;
-        alert('Você clicou no projeto: ' + nomeDoProjeto);
+        // id da imagem
+        const projetoId = link.getAttribute('data-project');
+        const fotoAlvo = document.getElementById(projetoId);
+
+        // verificação link ativo
+        const jaEstaAtivo = link.classList.contains('active');
+
+        // remove o - de todos os links e esconde todas as fotos
+        links.forEach(l => l.classList.remove('active'));
+        document.querySelectorAll('.project-content').forEach(foto => {
+            foto.style.display = 'none';
+        });
+
+        // abre a imagem se o link não tava ativo
+        if (!jaEstaAtivo) {
+            link.classList.add('active'); // O CSS muda o + para -
+            fotoAlvo.style.display = 'block'; // A foto aparece
+        }
+        // se já estava ativo, o código acima já fechou tudo
     });
-
 });
