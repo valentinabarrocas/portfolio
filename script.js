82cf558287f56bf49aad81623ee45fb37fcefe42
valentinabarrocas <valentinabarrocas@hotmail.com>
2026-02-08 15:09:47 -0300

    code refinement and links and descriptions adjustments

--- script.js
+++ script.js
@@ -4,16 +4,16 @@ const links = document.querySelectorAll('.project-link');
 // cria o loop
 links.forEach(link => {
     link.addEventListener('click', (e) => {
-        e.preventDefault(); // Impede a página de pular pro topo
+        e.preventDefault(); // impede a página de pular pro topo
 
         // id da imagem
         const projetoId = link.getAttribute('data-project');
         const fotoAlvo = document.getElementById(projetoId);
 
-        // verificação link ativo
+        // verifica link ativo
         const jaEstaAtivo = link.classList.contains('active');
 
-        // remove o - de todos os links e esconde todas as fotos
+        // remove - de todos os links e esconde todas as imagens
         links.forEach(l => l.classList.remove('active'));
         document.querySelectorAll('.project-content').forEach(foto => {
             foto.style.display = 'none';
@@ -21,9 +21,9 @@ links.forEach(link => {
 
         // abre a imagem se o link não tava ativo
         if (!jaEstaAtivo) {
-            link.classList.add('active'); // O CSS muda o + para -
-            fotoAlvo.style.display = 'block'; // A foto aparece
+            link.classList.add('active'); // css muda + para -
+            fotoAlvo.style.display = 'block'; // foto aparece
         }
-        // se já estava ativo, o código acima já fechou tudo
+        // se já estava ativo, o código acima fecha
     });
 });
